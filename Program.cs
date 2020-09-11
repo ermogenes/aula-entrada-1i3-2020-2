14d68b8ef0631908c6e130d815ac2e90be64dafd
Ermogenes Palacio <ermogenes@gmail.com>
2020-09-11 20:24:50 -0300

    Versão inicial do programa

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aula_entrada_1i3_2020_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeDoUsuario;
+            string sobrenomeDoUsuario;
+            string nomeCompleto;
+
+            Console.Write("Por favor, digite seu nome: ");
+            nomeDoUsuario = Console.ReadLine();
+
+            Console.Write("Agora digite seu sobrenome: ");
+            sobrenomeDoUsuario = Console.ReadLine();
+
+            nomeCompleto = $"{nomeDoUsuario} {sobrenomeDoUsuario}".ToUpper();
+
+            Console.WriteLine($"Obrigado, {nomeCompleto}! Pressione uma tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
